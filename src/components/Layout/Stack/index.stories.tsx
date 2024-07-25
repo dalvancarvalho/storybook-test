5bda0069033b9d54d9308fb96e9b4cdf52331023
Dalvan Carvalho <dalvan.bcarvalho@gmail.com>
2024-07-25 14:25:33 -0300

    update: colors

--- src/components/Layout/Stack/index.stories.tsx
+++ src/components/Layout/Stack/index.stories.tsx
@@ -18,9 +18,9 @@ type Story = StoryObj<typeof meta>
 export const Default: Story = {
   render: (args) => (
     <Stack className="gap-4 p-4 bg-gray-300" {...args}>
-      <Box className="w-[100px] h-[100px] bg-blue-500" />
-      <Box className="w-[100px] h-[100px] bg-red-500" />
-      <Box className="w-[100px] h-[100px] bg-green-500" />
+      <Box className="w-[100px] h-[100px] bg-purple-400" />
+      <Box className="w-[100px] h-[100px] bg-orange-400" />
+      <Box className="w-[100px] h-[100px] bg-emerald-400" />
     </Stack>
   ),
 }
